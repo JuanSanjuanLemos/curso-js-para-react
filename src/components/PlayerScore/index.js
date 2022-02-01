38d05058514df4852b780b6a2ad911348d64363e
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2022-02-01 14:54:10 -0300

    :sparkles: Create the PlayerScore
    #

---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function PlayerScore(Score){
+    return/*html*/`
+        <span class="player-score">
+            <div class="point -1"></div>
+            <div class="point -1"></div>
+            <div class="point -1"></div>
+        </span>
+    `;
+}
+
+export default PlayerScore;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,13 @@
 import "./style.css";
 import PlayerName from "../../components/PlayerName";
+import PlayerScore from "../../components/PlayerScore";
 
 function ScoreBoard(){
     return /*html*/`
         <header class=score-board>
             ${PlayerName("Player1")}
+            ${PlayerScore()}
+            ${PlayerScore()}
             ${PlayerName("Player2")}
         </header>
     `;
