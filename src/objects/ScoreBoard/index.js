085f94dce90dfdc2bcc7afcbb4d5f35ec4adeb9b
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2022-02-01 15:08:34 -0300

    :sparkles: Create the TextVS
    #

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,12 +1,14 @@
 import "./style.css";
 import PlayerName from "../../components/PlayerName";
 import PlayerScore from "../../components/PlayerScore";
+import TextVs from "../../components/TextVS";
 
 function ScoreBoard(){
     return /*html*/`
         <header class=score-board>
             ${PlayerName("Player1")}
             ${PlayerScore()}
+            ${TextVs()}
             ${PlayerScore()}
             ${PlayerName("Player2")}
         </header>
