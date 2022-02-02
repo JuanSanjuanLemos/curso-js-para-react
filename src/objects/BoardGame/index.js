04a9b5b161959be9895a59a020fb19db41e53b4a
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2022-02-02 07:30:22 -0300

    :sparkles: Create the Js,HTML and CSS cards
    #

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,18 +1,15 @@
 import "./style.css";
 import CardFrontBack from "../../components/CardFrontBack";
-
+import cards from "./data";
 
 function BoardGame(amountCards){
-    const $htmlFrontBack = CardFrontBack();
-    const $htmlContent = $htmlFrontBack.repeat(amountCards);
+    const htmlCardsList = cards.map((card) => CardFrontBack(card.icon,card.altIcon));
+    const $htmlCards = htmlCardsList.join('');
+
     return /*html*/`
     <section class='board-game'>
-        ${$htmlContent}
+        ${$htmlCards}
     </section>`;
 }
 
 export default BoardGame;
-/*<div>
-${PlayerScore(1)} ${PlayerScore(2)}
-${$htmlContent}
-</div>*/
